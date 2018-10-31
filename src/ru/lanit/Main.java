785dd0696a2f43360d07185ea41ae9864c988b09
mat179 <mat179@users.noreply.github.com>
2018-10-31 23:06:59 +0300

    Initial commit

---
+++ src/ru/lanit/Main.java
@@ -0,0 +1,84 @@
+package ru.lanit;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        long m = System.currentTimeMillis();
+       // generateFile();
+        File f = new File("random.txt");
+        minNumberFromFile(f);
+
+        //minNumberRandom();
+        System.out.println((System.currentTimeMillis()-m));
+    }
+
+    public static void minNumberRandom(){
+        Random random = new Random();
+        int count = random.nextInt(1000000);
+        boolean[] arr = new boolean[1000001];/*Set<Integer> set =*/
+        random.ints(-1000000000,1000000000)
+                .limit(count)
+                .filter(x -> (x>0)&&(x<=1000000))
+                .forEach(x ->arr[x] = true);
+
+        System.out.println(getMinFromArray(arr));
+
+    }
+
+
+    public static void  minNumberFromFile(File f){
+        try (Scanner sc = new Scanner(new FileReader(f))/*BufferedReader reader = new BufferedReader(new FileReader(f))*/) {
+            List<Integer> list = new LinkedList<>();
+            if (!f.exists())
+                throw new FileNotFoundException();
+            sc.useDelimiter(" ");
+            while (sc.hasNext()){
+                list.add(sc.nextInt());
+            }
+            boolean[] arr = new boolean[1000001];
+            list.stream()
+                 .filter(x -> (x>0)&&(x<=1000000))
+                 .forEach(x ->arr[x] = true);
+
+            System.out.println(getMinFromArray(arr));
+        } catch (FileNotFoundException e){
+            System.out.println("Файл не найден");
+        }
+    }
+
+    public static int getMinFromArray(boolean[] arr){
+        for (int i = 1; i<arr.length; i++) {
+            if (!arr[i]){
+                return i;
+            }
+        }
+        return 1000001;
+    }
+
+    public static void generateFile(){
+        File f = new File("random.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(f))){
+            if (f.exists())
+                f.delete();
+            f.createNewFile();
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            int count =random.nextInt(1000000);
+            random.ints(-1000000000,1000000000)
+                    .limit(count)
+                    .forEach(x -> builder.append(x+" "));
+            writer.append(builder);
+            writer.flush();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
